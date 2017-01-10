cb43acd77c3867d0ace3d52c5d8dfbe497849662
mjawath <mjawath@gmail.com>
2017-01-10 06:21:09 +0530

    base

---
+++ EntityBase/src/main/java/com/mycompany/entitybase/dao/BaseDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.entitybase.dao;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author LENOVO PC
+ */
+public interface BaseDAO<T, ID extends Serializable>  {
+    
+    
+    
+}
